784e0d0b21ae7f3e56ec300e2d84fbf2f25941ee
ayyf <yyfu@youzu.com>
2018-11-19 15:29:32 +0800

    添加MobPushWrapper

---
+++ proj.android-studio/c2dxbridge/src/main/java/com/mob/mobpush/cocos2dx/MobPushWrapper.java
@@ -0,0 +1,35 @@
+package com.mob.mobpush.cocos2dx;
+
+import android.text.TextUtils;
+
+import com.mob.MobSDK;
+import com.mob.pushsdk.MobPush;
+import com.mob.pushsdk.MobPushLocalNotification;
+import com.mob.tools.utils.DeviceHelper;
+import com.mob.tools.utils.ResHelper;
+
+import java.util.Random;
+
+import static com.mob.MobSDK.getContext;
+
+
+public class MobPushWrapper {
+
+	public static void sendLocalNotify(String content, int space){
+		MobPushLocalNotification notification = new MobPushLocalNotification();
+		String appName = DeviceHelper.getInstance(getContext()).getAppName();
+		notification.setTitle(TextUtils.isEmpty(appName) ? getContext().getString(R.string.item_local) : appName);
+		notification.setContent(content);
+//				notification.setVoice(false);//可设置不进行声音提醒，默认声音、振动、指示灯
+		notification.setNotificationId(new Random().nextInt());
+		notification.setTimestamp(space * 60 * 1000 + System.currentTimeMillis());
+		MobPush.addLocalNotification(notification);
+	}
+
+	public void setNotifyIcon(String resIcon){
+		if(!TextUtils.isEmpty(resIcon)){
+			MobPush.setNotifyIcon(ResHelper.getBitmapRes(MobSDK.getContext(), resIcon));
+		}
+	}
+
+}
